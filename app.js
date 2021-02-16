96cd5cc31eef225a3c5176090720439d850f10e8
Eddie <github.towage@aleeas.com>
2021-02-16 10:37:32 -0800

    added weather text and image

--- app.js
+++ app.js
@@ -12,11 +12,17 @@ app.get('/', function(req, res){
     response.on('data', function(data){
       const weatherData = JSON.parse(data)
       const temp = weatherData.main.temp
-      const description = weatherData.weather[0].description; 
+      const description = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+      res.write("<h1>The weather is currently " + description + "</h1>")
+      res.write("<h1>The temperature in Boston is " + temp + " degrees Farenheit </h1>")
+      res.write("<img src=" + imageURL + ">")
+      res.send();
     })
   })
 
-  res.send("Server is up and running")
 
 })
 
